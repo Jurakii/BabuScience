6b630560a1dfe7b8b3ec6062f56939df8dca219d
Jurakii <ncrouse518@gmail.com>
2022-05-17 09:27:52 -0700

    Update funids.js

--- Projects/funids/funids.js
+++ Projects/funids/funids.js
@@ -4,43 +4,60 @@ var ctx = canvas.getContext("2d");
 var playerY = 0;
 var playerX = 300;
 var velocity = 0;
+var objectX = canvas.width;
 document.addEventListener("keydown", keyDown, false);
 document.addEventListener("keyup", keyUp, false);
-//document.addEventListener("touchstart", handleStart, false);
-//document.addEventListener("touchend", handleEnd, false);
+document.addEventListener("touchstart", handleStart, false);
+document.addEventListener("touchend", handleEnd, false);
 
 function keyDown(e) {
-    if(e.keyCode == 32){
+    if (e.keyCode == 32) {
         space = true;
     }
 }
 
 function keyUp(e) {
-    if(e.keyCode == 32){
+    if (e.keyCode == 32) {
         space = false;
     }
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function handleStart() {
+    space = true;
+}
+
+function handleEnd() {
+    space = false;
+}
+
+function player() {
+
     ctx.beginPath();
     ctx.rect(playerX, playerY, 50, 50);
-    ctx.stroke();
     ctx.fillStyle = "#FF0000";
     ctx.fill();
     ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player();
+    ctx.beginPath();
+    ctx.rect(objectX, 700, 50, 50);
+    ctx.fillStyle = "#0000FF";
+    ctx.fill();
+    ctx.closePath();
+    objectX -= 5;
     playerY += velocity;
     velocity += 0.25;
     if (playerY >= 700) {
         playerY = 700;
     }
-    if (left) {
-        playerX -= 5;
-    } else if (right){
-        playerX += 5;
+    if (space) {
+        velocity = -6;
     }
-    if(space){
-        velocity = -10;
+    if (objectX <= -50) {
+        objectX = canvas.width;
     }
 }
 
