9b632cff6882701968a14d86b764dafd6822e01e
Jurakii <ncrouse518@gmail.com>
2022-05-24 12:38:06 -0700

    Animation Added

--- Projects/funids/FunidsFlip.js
+++ Projects/funids/FunidsFlip.js
@@ -12,7 +12,14 @@ var floor = canvas.height;
 var roof = 0;
 var space = false;
 var gravity = 1;
-
+var drawing = null;
+var column = 8;
+var frameHeight = 0;
+var frameWidth = 0;
+var frame = 0;
+var ticks = 0;
+var flipped = false;
+var offset = 0;
 document.addEventListener("keydown", keyDown, false);
 document.addEventListener("keyup", keyUp, false);
 document.addEventListener("touchstart", handleStart, false);
@@ -49,7 +56,7 @@ function handleEnd() {
 
 function player() {
     ctx.beginPath();
-    ctx.rect(playerX, playerY, 50, 50);
+    ctx.rect(playerX, playerY - 150, 100, 160);
     ctx.fillStyle = "#FF0000";
     ctx.fill();
     ctx.closePath();
@@ -84,31 +91,57 @@ function collision() {
         playerY = 0;
     }
 }
+function animation(){
+    drawing = new Image();
+    if(flipped){
+        drawing.src = "../Projects/funids/Running/RunningFlipped.png";
+offset = 155;
+    } else {
+    drawing.src = "../Projects/funids/Running/Running.png";
+        offset = 175;
+    }
+    frameWidth = drawing.width / column;
+    frameHeight = drawing.height;
 
 
+ctx.drawImage(drawing, frame, 0, frameWidth, frameHeight, playerX, playerY-offset, frameWidth/10,frameHeight/10 );
+}
 function draw() {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight - 100;
+    ctx.canvas.width = window.innerWidth * .75;
+    ctx.canvas.height = window.innerHeight * .75;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     floor = canvas.height - 50;
-    player();
+    //player();
     //object(1);
     playerY -= velocity * gravity;
     velocity -= 0.25;
     collision();
 
-    if (playerY >= floor && gravity == 1) {
-        playerY = floor;
+    animation();
+    if(ticks == 20 || ticks == 40 || ticks == 60 || ticks == 80 || ticks == 100 || ticks == 120 || ticks == 140 || ticks == 160){
+    frame += frameWidth;
+    if(frame == drawing.width){
+        frame = 0;
+    }}
+    ticks += 2;
+    if(ticks >= 81){
+        ticks = 0;
+    }
+    if (playerY >= floor + 40 && gravity == 1) {
+        playerY = floor + 40;
         grounded = true;
         velocity = 0;
-    } else if (gravity == -1 && playerY <= roof) {
-        playerY = roof;
+        flipped = false;
+    } else if (gravity == -1 && playerY <= roof + 150) {
+        playerY = roof + 150;
         grounded = true;
         velocity = 0;
+        flipped = true;
     }
     if (space && grounded) {
-        velocity += 8;
+        velocity += 10;
         grounded = false;
+
     }
 }
-setInterval(draw, 10);
+setInterval(draw, 5);
